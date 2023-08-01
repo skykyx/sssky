5ae311bd611a3996cb4a60f1bbfe3a6c248a1af3
Night God <12237306@qq.com>
2023-08-01 17:06:36 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ddd680f7-8ef3-479f-be6d-08688ec23191';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
